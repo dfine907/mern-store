deddeae7c2a85300dc307984a39616fdca5728b9
Dina <dinagiacobbi@gmail.com>
2024-12-25 10:56:02 -0900

    Adding delete method API

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,18 @@ app.post('/api/products', async (req, res) => {
     }
 })
 
+app.delete('/api/products/:id', async (req, res) => {
+const { id } = req.params
+
+try {
+  await Product.findByIdAndDelete(id)
+  res.status(200).json( {success: true, message: "Product was deleted"})
+} catch (error) {
+  
+}
+
+})
+
 app.listen(5000, () => {
   connectDB()
   console.log('Server listening on port 5000...')
