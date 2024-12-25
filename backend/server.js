81881e9c6041ce132003fef2869e34987fb0d9e3
Dina <dinagiacobbi@gmail.com>
2024-12-25 12:16:29 -0900

    Adding update method

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import Product from './models/product.js'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
@@ -9,38 +10,76 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({})
+    res
+      .status(200)
+      .json({ success: true, products, count: products.length })
+  } catch (error) {
+    console.log('Error in getting products: ', error.message)
+    res.status(500).json({ success: false, message: 'Server error' })
+  }
+})
+
 app.post('/api/products', async (req, res) => {
   const product = req.body
 
   if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({
-        success: false,
-        messsage: 'please fill out all fields',
-      })
+    return res.status(400).json({
+      success: false,
+      messsage: 'please fill out all fields',
+    })
   }
   const newProduct = new Product(product)
 
-    try {
-        await newProduct.save()
-        res.status(201).json({success: true, data: newProduct} )
-    } catch (error) {
-        console.log("Error in creating product: ", error.messsage)
-        res.status(500).json( {success:false, message: "Server error"} )
-    }
+  try {
+    await newProduct.save()
+    res.status(201).json({ success: true, data: newProduct })
+  } catch (error) {
+    console.log('Error in creating product: ', error.messsage)
+    res.status(500).json({ success: false, message: 'Server error' })
+  }
 })
 
 app.delete('/api/products/:id', async (req, res) => {
-const { id } = req.params
+  const { id } = req.params
 
-try {
-  await Product.findByIdAndDelete(id)
-  res.status(200).json( {success: true, message: "Product was deleted"})
-} catch (error) {
-  
-}
+  try {
+    await Product.findByIdAndDelete(id)
+    res
+      .status(200)
+      .json({ success: true, message: 'Product was deleted' })
+  } catch (error) {
+    console.log('Error in deleting product: ', error.message)
+    res
+      .status(404)
+      .json({ success: false, message: 'Your product was not found' })
+  }
+})
 
+app.patch('/api/products/:id', async (req, res) => {
+  const { id } = req.params
+
+  const product = req.body
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: 'Product not found' })
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      product,
+      { new: true }
+    )
+    res.status(200).json({ success: true, data: updatedProduct })
+  } catch (error) {
+    console.log('Error in updating product: ', error.message)
+    res.status(500).json({ success: false, message: 'Server Error' })
+  }
 })
 
 app.listen(5000, () => {
