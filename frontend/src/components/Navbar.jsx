ac649f070025a3c1af59e2b74b3353d1d24b0610
Dina <dinagiacobbi@gmail.com>
2024-12-30 08:17:27 -0900

    Adding toggle color mode

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,48 +1,52 @@
-import { Box, Button, Container, Flex, Text, HStack } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import {
+  Button,
+  Container,
+  Flex,
+  Text,
+  HStack,
+  useColorMode,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { FaPlusSquare } from 'react-icons/fa';
+import { MdOutlineLightMode } from 'react-icons/md';
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Box w='100%' h='200px' bgGradient='linear(to-r, green.200, pink.500)'> 
-
-      <Container maxW={'1140px'} px={4}>
-        <Flex
-          h={16}
-          alignItems={'center'}
-          justifyContent={'space-between'}
-          flexDir={{
-            base: 'column',
-            sm: 'row',
-          }}
+    <Container maxW={'1140px'} px={4}>
+      <Flex
+        h={16}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexDir={{
+          base: 'column',
+          sm: 'row',
+        }}
+      >
+        <Text
+          fontSize={{ base: '22', sm: '28' }}
+          fontWeight={'bold'}
+          textTransform={'uppercase'}
+          textAlign={'center'}
+          color={'purple.400'}
         >
-          <Text
-            fontSize={{ base: '22', sm: '28' }}
-            fontWeight={'bold'}
-            textTransform={'uppercase'}
-            textAlign={'center'}
-            // bgGradient={'linear(to-r, cyan.400, blue.500)'}
-            // bgClip={'text'}
-           color={'purple.400'}
-          >
-            <Link to={'/'}>Product Store 🛒</Link>
-          </Text>
-          <HStack spacking={2} alignItems={'center'}>
-            <Link to={"/create"}>
+          <Link to={'/'}>Product Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={'center'}>
+          <Link to={'/create'}>
             <Button>
-            Button
+              <FaPlusSquare />
             </Button>
-            </Link>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            <MdOutlineLightMode />
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
 
-          </HStack>
-
-
-        </Flex>
-      </Container>
-
-      </Box>
-    
-  )
-}
-
-export default Navbar
+export default Navbar;
